b73f04b27efe1b574274f661e3d34f8d17cd9241
tinithi-perera <tinithiperera@gmail.com>
2026-08-05 11:56:54 +0530

    implement sendPromptOnAction function and update index.html with input elements

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("hi");
+function sendPromptOnAction() {
+    alert("Prompt sent to the server!");
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("X-goog-api-key", " api key here");
+    let userInput = document.getElementById("txtUserInput").value;
+    const raw = JSON.stringify({
+        "contents": [
+            {
+                "parts": [
+                    {
+                        "text": userInput
+                    }
+                ]
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent", requestOptions)
+        .then((response) => response.json())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
+
+
+}
